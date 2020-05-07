9eb43bdd145423645125c85b4e3373bb1fa96aed
lethanh11 <lethanh110700@gmail.com>
2020-05-07 14:11:04 +0700

    nộp bài

---
+++ th1p3.js
@@ -0,0 +1,9 @@
+let a = +prompt("Nhập a ");
+let b = +prompt("Nhập b");
+let c = a%b;
+if (c==0) {
+    document.write("a là bội số của b");
+} else {
+    document.write("a không phải là bội số của b");
+}
+
