fb3fe1b9f0e2e389515114e10ff48134011234bf
lethanh11 <lethanh110700@gmail.com>
2020-05-07 17:05:51 +0700

    consver

---
+++ bt5.js
@@ -0,0 +1,10 @@
+function moneychane(){
+var sotienInput= document.getElementById('sotien').value;
+var sotienOutput=0;
+var loaitienInput= document.getElementById('from').value;
+var loaitienOutput= document.getElementById('to').value;
+if(loaitienOutput==loaitienInput) sotienOutput=sotienInput;
+if((loaitienInput=="VND") && (loaitienOutput=="USD")) {sotienOutput= sotienInput/23000;}
+if((loaitienInput=="USD") && (loaitienOutput=="VND")) {sotienOutput= sotienInput*23000;}
+document.getElementById('Result').innerHTML="Result =" + sotienOutput + " "+ loaitienOutput;       
+}   
